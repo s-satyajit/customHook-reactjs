49dcbcd146bb5ad3955fc3d15b57a3e41eb14c0a
Satyajit Samal <samalsatyajit668@gmail.com>
2024-10-12 18:47:07 +0530

    used the custom hook as an input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from "react"
+import { useState } from "react"
+import useCurrencyInfo from "./Hooks/useCurrencyInfo"
 
 function App() {
+  const [currency, setCurrency] = useState('')
+
+  // currencyOption = []
+  const currencyInfo = useCurrencyInfo('')
+  const options = Object.keys(currencyInfo)
 
   return (
     <>
-      
+      <div>
+        <input 
+        type="text" 
+        value={currencyInfo}
+        currencyOption = {options}
+        onChange={(e) => {setCurrency(e.target.value)}}
+        />
+        <select>
+          {options.map((currency) => {
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          })}
+        </select>
+      </div>
     </>
   )
 }
